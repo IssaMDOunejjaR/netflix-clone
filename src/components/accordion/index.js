d6315c6300e07aaf6cf84de4d5b1219dd1ed083f
IssaMDOunejjaR <otaku6issam@gmail.com>
2021-08-18 23:45:27 +0100

    Finished the accordion section

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -25,7 +25,7 @@ Accordion.Item = function AccordionItem({ children, ...restProps }) {
 	const [toggleShow, setToggleShow] = useState(false);
 
 	return (
-		<ToggleContext.Provider value={toggleShow, setToggleShow}>
+		<ToggleContext.Provider value={{toggleShow, setToggleShow}}>
 			<Item {...restProps}>{children}</Item>
 		</ToggleContext.Provider>
 	);
@@ -34,7 +34,12 @@ Accordion.Item = function AccordionItem({ children, ...restProps }) {
 Accordion.Header = function AccordionHeader({ children, ...restProps }) {
 	const {toggleShow, setToggleShow} = useContext(ToggleContext);
 
-	return <Header onClick={() => setToggleShow((toggleShow) => !toggleShow)}{...restProps}>{children}</Header>;
+	return (
+		<Header onClick={() => setToggleShow((toggleShow) => !toggleShow)} {...restProps}>
+			{children}
+			{toggleShow ? (<img src="/images/icons/close-slim.png" alt="Close" />) : (<img src="/images/icons/add.png" alt="Open" />)}
+		</Header>
+	);
 };
 
 Accordion.Body = function AccordionBody({ children, ...restProps }) {
